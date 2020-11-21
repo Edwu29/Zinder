33f5e5e9f465c82d73189bce5806092a546fd839
Edwu29 <edwardwu10@gmail.com>
2020-11-20 16:41:52 -0800

    heroku

---
+++ build/precache-manifest.bac73fe00ee37ee8a83dbafc0af3e4d9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22c14e18ee2b705eaebe00b0081005be",
+    "url": "/Zinder/index.html"
+  },
+  {
+    "revision": "a193cf7cd46514376a86",
+    "url": "/Zinder/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "d4331acbe5c673b3a838",
+    "url": "/Zinder/static/css/main.8d7c7e33.chunk.css"
+  },
+  {
+    "revision": "a193cf7cd46514376a86",
+    "url": "/Zinder/static/js/2.058de1f1.chunk.js"
+  },
+  {
+    "revision": "a8b15fcc633b7864f2dc19d56211bd16",
+    "url": "/Zinder/static/js/2.058de1f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4331acbe5c673b3a838",
+    "url": "/Zinder/static/js/main.fba2929d.chunk.js"
+  },
+  {
+    "revision": "3f5d30330ee57b1170d0",
+    "url": "/Zinder/static/js/runtime-main.c8b2999d.js"
+  }
+]);
