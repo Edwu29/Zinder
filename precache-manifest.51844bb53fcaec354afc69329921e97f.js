73a783a6b6554935b409863e4cfb3c7e6eaebf63
Edwu29 <edwardwu10@gmail.com>
2020-11-20 17:58:39 -0800

    Updates

---
+++ precache-manifest.51844bb53fcaec354afc69329921e97f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b14f16f302966bc188715cb97787234",
+    "url": "/Zinder/index.html"
+  },
+  {
+    "revision": "a193cf7cd46514376a86",
+    "url": "/Zinder/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "fd39a57c80cb645eb370",
+    "url": "/Zinder/static/css/main.8d7c7e33.chunk.css"
+  },
+  {
+    "revision": "a193cf7cd46514376a86",
+    "url": "/Zinder/static/js/2.058de1f1.chunk.js"
+  },
+  {
+    "revision": "a8b15fcc633b7864f2dc19d56211bd16",
+    "url": "/Zinder/static/js/2.058de1f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd39a57c80cb645eb370",
+    "url": "/Zinder/static/js/main.4356868d.chunk.js"
+  },
+  {
+    "revision": "3f5d30330ee57b1170d0",
+    "url": "/Zinder/static/js/runtime-main.c8b2999d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6ee3c2e6b6e0f2044b0d2588c01d4df5.js"
+  "/Zinder/precache-manifest.51844bb53fcaec354afc69329921e97f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Zinder/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
